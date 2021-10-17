965b11cf775bb8d1b4efa02bbe8a8f0b96e2a8ea
hanryu1109 <hanryu1109@gmail.com>
2021-10-17 23:48:42 +0900

    Update promise.js

--- async/promise.js
+++ async/promise.js
@@ -13,3 +13,51 @@ const promise = new Promise((resolve, reject) => {
         resolve('ellie');
     }, 2000);
 });
+
+// 2. Consumers: then, catch, finally
+promise //
+  .then(value => {
+    console.log(value);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log('finally');
+  });
+
+// 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+  .then(num => num * 2)
+  .then(num => num * 3)
+  .then(num => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(num - 1), 1000);
+    });
+  })
+  .then(num => console.log(num));
+
+// 4. Error Handling
+const getHen = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve('🐓'), 1000);
+  });
+const getEgg = hen =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error(`error! ${hen} => 🥚`)), 1000);
+  });
+const cook = egg =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${egg} => 🍳`), 1000);
+  });
+
+getHen() //
+  .then(getEgg)
+  .then(cook)
+  .then(console.log)
+  .catch(console.log);
+  
